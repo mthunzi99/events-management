c60392038e7b17fced97a07a7c44798ad8135f62
mthunzi <mthunzi99@gmail.com>
2026-06-02 14:01:41 +0200

    Minor edits

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -1,19 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import client from "@/api/client";
 import { peopleColumns } from "../../columns";
-import { People, useAttendees } from "@/components/context/AttendeeProvider";
-import { EventType, useEvent } from "@/components/context/EventProvider";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { useAttendees } from "@/components/context/AttendeeProvider";
+import { useEvent } from "@/components/context/EventProvider";
 import { DataTable } from "@/components/ui/data-table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PrintsChart } from "@/components/PrintsChart";
@@ -27,31 +16,29 @@ export default function Events() {
       <h1 className="text-3xl font-bold mb-6">
         {activeEvent?.event || "Select an Event"}
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-10">
-        <PrintsChart />
-        <div className="flex flex-col space-y-4">
-          <Card className="h-full">
-            <CardHeader>
-              <CardTitle className="text-xl font-semibold mb-2">
-                Event Details
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="text-sm flex flex-col space-y-2">
-              <p>Venue: {activeEvent?.venue || "No description available."}</p>
-              <p>
-                Dates:{" "}
-                {activeEvent?.from_date
-                  ? `${new Date(activeEvent.from_date).toLocaleDateString()} - ${new Date(activeEvent.to_date || activeEvent.from_date).toLocaleDateString()}`
-                  : "No dates available."}
-              </p>
-            </CardContent>
-          </Card>
-          <Card className="h-full flex flex-col space-y-2">
-            <CardContent className="text-sm">
-              <p>Total Attendees: {attendees.length}</p>
-            </CardContent>
-          </Card>
-        </div>
+      <PrintsChart />
+      <div className="flex flex-direction-row space-x-4 py-4">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="text-xl font-semibold mb-2">
+              Event Details
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="text-sm flex flex-col space-y-2">
+            <p>Venue: {activeEvent?.venue || "No description available."}</p>
+            <p>
+              Dates:{" "}
+              {activeEvent?.from_date
+                ? `${new Date(activeEvent.from_date).toLocaleDateString()} - ${new Date(activeEvent.to_date || activeEvent.from_date).toLocaleDateString()}`
+                : "No dates available."}
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="w-full flex flex-col space-y-2">
+          <CardContent className="text-sm">
+            <p>Total Attendees: {attendees.length}</p>
+          </CardContent>
+        </Card>
       </div>
       <div className="py-6">
         <h1 className="text-3xl font-bold mb-6">
